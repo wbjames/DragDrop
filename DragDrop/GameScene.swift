70044ece472486b0bcbe701a53be125844f7e836
wbjames <314771755@qq.com>
2014-07-20 22:49:46 +0800

    touch and gesture in Sprite Kit, just to exam

--- DragDrop/GameScene.swift
+++ DragDrop/GameScene.swift
@@ -1,44 +1,163 @@
-//
-//  GameScene.swift
-//  DragDrop
-//
-//  Created by wubin on 7/20/14.
-//  Copyright (c) 2014 wubin. All rights reserved.
-//
-
 import SpriteKit
+import UIKit
+
+let kAnimalNodeName:String = "movable"
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    var background:SKSpriteNode = SKSpriteNode()
+    var selectedNode:SKSpriteNode = SKSpriteNode()
+    
+    
+    
+    init(size:CGSize){
+        super.init(size: size)
+        background = SKSpriteNode(imageNamed: "blue-shooting-stars.png")
+        background.name = "background"
+        background.anchorPoint = CGPointMake(0.0, 0.0)
+        self.addChild(background)
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+        // loading the images
+        let imageNames:NSArray = ["bird", "cat", "dog", "turtle"]
+        for i in 0 ..< imageNames.count {
+            let imageNamed:String = imageNames[i] as String
+            let sprite = SKSpriteNode(imageNamed: imageNamed)
+            sprite.name = kAnimalNodeName
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let offsetFraction = ((CGFloat)(i+1) / (CGFloat)(imageNames.count + 1)) as CGFloat
+            sprite.position = CGPointMake(size.width * offsetFraction, size.height / 2)
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            background.addChild(sprite)
         }
     }
+    
+    
+    
+    
+    override func didMoveToView(view: SKView) {
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePanFrom:")
+        self.view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func handlePanFrom(recognizer:UIPanGestureRecognizer) {
+        if recognizer.state == UIGestureRecognizerState.Began {
+            var touchLocation = recognizer.locationInView(recognizer.view)
+            
+            touchLocation = self.convertPointFromView(touchLocation)
+            
+            self.selectNodeForTouch(touchLocation)
+        } else if recognizer.state == UIGestureRecognizerState.Changed {
+            
+            var translation = recognizer.translationInView(recognizer.view)
+            translation = CGPointMake(translation.x, -translation.y)
+            self.panForTranslation(translation)
+            recognizer.setTranslation(CGPointZero, inView: recognizer.view)
+            
+        } else if recognizer.state == UIGestureRecognizerState.Ended {
+            let scrollDuration = 0.2
+            if selectedNode.name != kAnimalNodeName {
+                let velocity = recognizer.velocityInView(recognizer.view)
+                let pos = selectedNode.position
+                let p = mult(velocity, s: scrollDuration)
+                
+                var newPos = CGPointMake(pos.x + p.x, pos.y + p.y)
+                newPos = boundLayerPos(newPos)
+                
+                
+                selectedNode.removeAllActions()
+                
+                let moveTo = SKAction.moveTo(newPos, duration: NSTimeInterval(scrollDuration))
+                moveTo.timingMode = SKActionTimingMode.EaseOut
+                selectedNode.runAction(moveTo)
+                
+            }
+            
+        }
+        
+    }
+    
+    func mult(v: CGPoint, s:CGFloat) -> CGPoint{
+        return CGPointMake(v.x*s, v.y*s)
+    }
+    
+    
+    
+    // 按下去
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+
+//        let touch:UITouch = touches.anyObject() as UITouch
+//        let positionInScene = touch.locationInNode(self)
+//        selectNodeForTouch(positionInScene)
+    }
    
+   
+    
+    func selectNodeForTouch(touchLocation: CGPoint) {
+        let touchedNode = self.nodeAtPoint(touchLocation) as SKSpriteNode
+        
+        if !touchedNode.isEqual(selectedNode) {
+
+            selectedNode.removeAllActions()
+            selectedNode.runAction(SKAction.rotateToAngle(0.0, duration: NSTimeInterval(0.1)))
+
+            
+            selectedNode = touchedNode
+            
+            if touchedNode.name == kAnimalNodeName {
+                let sequcene = [SKAction.rotateByAngle(degToRad(-4.0), duration: NSTimeInterval(0.1)),
+                                SKAction.rotateByAngle(0.0, duration: NSTimeInterval(0.1)),
+                                SKAction.rotateByAngle(degToRad(4.0), duration: NSTimeInterval(0.1))]
+                selectedNode.runAction(SKAction.repeatActionForever(SKAction.sequence(sequcene)))
+
+            }
+            
+        }
+        
+    }
+    
+    func degToRad(degree : CGFloat) -> CGFloat{
+        return degree / 180 * M_PI
+    }
+    
+    
+    
+    
+    // 移动
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+//        let touch = touches.anyObject() as UITouch
+//        let positionInScene = touch.locationInNode(self)
+//        let previouePosition = touch.previousLocationInNode(self)
+//        
+//        let translation = CGPointMake(positionInScene.x - previouePosition.x, positionInScene.y - previouePosition.y)
+//        panForTranslation(translation)
+    }
+    
+    func boundLayerPos(newPos: CGPoint) -> CGPoint{
+        let winSize = self.size
+        var retVal = newPos
+        retVal.x = min(retVal.x, 0)
+        retVal.x = max(retVal.x, winSize.width - background.size.width)
+        retVal.y = self.position.y
+        
+        return retVal
+    }
+    
+    func panForTranslation(translation: CGPoint){
+
+        let position = selectedNode.position
+        if selectedNode.name == kAnimalNodeName {
+            selectedNode.position = CGPointMake(position.x + translation.x, position.y + translation.y)
+        } else {
+            let newPos = CGPointMake(position.x + translation.x, position.y + translation.y)
+            background.position = boundLayerPos(newPos)
+        }
+        
+    }
+    
+
+    
+    
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
